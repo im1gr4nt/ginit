43d88329ba5e736c49a8c516106131e8d1705de4
Dawid <januch1993@gmail.com>
2022-01-11 16:34:14 +0100

    added .gitignore

--- ginit.sh
+++ ginit.sh
@@ -3,12 +3,12 @@
 url=$1
 repo_name=${url##*/}
 
-git clone $url
 if [[ -z $url ]]; then
 	#tbd insert help lines or cat help file
 	echo "Repository URL is missing"
 	echo "Try again and provide repository URL"
-
-cd $repo_name
-git init
-
+else
+	git clone $url < credentials.txt
+	cd $repo_name
+	git init
+fi
